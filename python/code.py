805c6744e6e4f39d5ba6d52fccc0142a71a93cbe
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-12-11 09:16:38 -0500

    init commit added folders and code for hello world

---
+++ python/code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Created: By Jakub Malhotra
+Created: On December 2024
+This is my "VTEC Velocity" game on the Adafruit Pybadge
+"""
+
+import stage
+import ugame
+
+
+def game_scene() -> None:
+    """
+    This is the main function game scene
+    """
+
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass  # Just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
